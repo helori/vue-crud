aa011c685743997b66e9fd7d068cb82192e2da87
helori <helori.lanos@gmail.com>
2017-09-05 13:08:14 +0200

    Date filter format fix

--- src/filters/FilterDate.js
+++ src/filters/FilterDate.js
@@ -1,6 +1,6 @@
 Vue.filter('date', function (value, format) {
     if(value){
-        return moment(value, "YYYY-MM-DD hh:mm:ss").format('DD/MM/YYYY');
+        return moment(value, "YYYY-MM-DD hh:mm:ss").format(format);
     }else{
         return '';
     }
@@ -8,7 +8,7 @@ Vue.filter('date', function (value, format) {
 
 Vue.filter('datetime', function (value, format) {
     if(value){
-        return moment(value, "YYYY-MM-DD hh:mm:ss").format('DD/MM/YYYY à hh:mm');
+        return moment(value, "YYYY-MM-DD hh:mm:ss").format(format);
     }else{
         return '';
     }
