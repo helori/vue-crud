4198a9c3297e3846176dd842ea61257264b0b77e
helori <helori.lanos@gmail.com>
2017-09-26 14:59:59 +0200

    Date input css fix

--- src/inputs/InputDate.vue
+++ src/inputs/InputDate.vue
@@ -1,13 +1,13 @@
-<style scoped>
+<style>
 .vdp-datepicker.form-control{
     padding: 3px;
 }
 .vdp-datepicker.form-control input{
     width: 100%;
     border: 0;
-    padding: 0 5px;
-    height: 32px;
-    line-height: 32px;
+    padding: 0 9px;
+    height: 30px;
+    line-height: 30px;
 }
 </style>
 
@@ -17,6 +17,7 @@
         v-model="dataValue"
         format="dd MMMM yyyy"
         class="form-control"
+        :class="'text-' + textAlign"
         :full-month-name="true"
         :monday-first="true"
         language="fr"
@@ -42,6 +43,13 @@
                 debounceDelay: 1000
             };
         },
+        props: {
+            textAlign: {
+                type: String,
+                default: 'left',
+                required: false
+            }
+        },
         watch: {
             value: {
                 handler: function (val) {
@@ -54,37 +62,6 @@
                 var d = moment(val).format('YYYY-MM-DD') + ' 00:00:00';
                 return d;
             }
-
-            /*formatDisplayValue(value) {
-                var d = value.substring(0, 2);
-                var m = value.substring(3, 5);
-                var y = value.substring(6, 10);
-                var val = d + '/' + m + '/' + y;
-                return val;
-            },
-
-            formatUpdateValue(value) {
-                var d = value.substring(0, 2);
-                var m = value.substring(3, 5);
-                var y = value.substring(6, 10);
-                var val = y + '-' + m + '-' + d + ' 00:00:00';
-                return val;
-            },
-
-            validateValue(val){
-                var value = val.trim();
-                var valid = true;
-                var d = parseInt(value.substring(0, 2));
-                var m = parseInt(value.substring(3, 5));
-                var y = parseInt(value.substring(6, 10));
-                if(value.length !== 10){
-                    valid = false;
-                }
-                else if(isNaN(d) || isNaN(m) || isNaN(y) || d < 1 || d > 31 || m < 1 || m > 12){
-                    valid = false;
-                }
-                return valid;
-            }*/
         }
     }
 </script>
