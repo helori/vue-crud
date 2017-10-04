2fa424118136c87cc53a0f339cd82c6bf588d61e
helori <helori.lanos@gmail.com>
2017-10-04 11:21:00 +0200

    Number formatting using numeraljs

--- src/filters/FilterNumber.js
+++ src/filters/FilterNumber.js
@@ -1,31 +1,36 @@
 import Vue from 'vue'
+import numeral from 'numeral'
+
+numeral.register('locale', 'fr', {
+    delimiters: {
+        thousands: ' ',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '€'
+    }
+});
+
+// switch between locales
+numeral.locale('fr');
+
 
 Vue.filter('number', function (value, decimals) {
-    if(value){
-        var ent = Math.floor(value);
-        var dec = 0;
-        if(ent != value){
-            dec = (value - ent) + '';
-            dec = dec.substring(2);
-            dec = dec.substring(0, decimals ? parseInt(decimals) : 0);
+    var format = '0,0';
+    if(decimals > 0){
+        format += '.';
+        for(var i=0; i<parseInt(decimals); ++i){
+            format += '0';
         }
-        ent = ent + '';
-        
-        var result = '';
-        var counter = 0;
-        for(var i=ent.length - 1; i>=0; --i){
-            if(counter == 3){
-                result += ' ';
-                counter = 0;
-            }
-            result = ent.charAt(i) + result;
-            ++counter;
-        }
-        if(dec){
-            result += ',' + dec;
-        }
-        return result;
-    }else{
-        return '';
     }
+    return numeral(value).format(format);
 });
