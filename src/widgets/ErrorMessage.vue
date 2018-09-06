c33dba048ad231f851427a00964ac57f426116b5
helori <helori.lanos@gmail.com>
2018-09-06 12:00:53 +0200

    Error message widget added

---
+++ src/widgets/ErrorMessage.vue
@@ -0,0 +1,30 @@
+<style>
+</style>
+
+<template>
+    <div class="alert alert-danger mt-2 mb-0" v-if="errors">
+        <div v-if="errors.errors">
+            <div v-for="(errors, field) in errors.errors">
+                <div v-for="error in errors">
+                    <i class="fal fa-arrow-circle-right"></i>
+                    {{ error }}
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            {{ errors.message }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+	export default {
+
+    	props: {
+            errors: {
+                required: true
+            }
+        }
+    }
+</script>
