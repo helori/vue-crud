d51e8358086b7bd8fae16f70b9e7e42fc44ebd0a
helori <helori.lanos@gmail.com>
2019-01-08 16:54:38 +0100

    Update FieldsDrawer.vue

--- src/widgets/FieldsDrawer.vue
+++ src/widgets/FieldsDrawer.vue
@@ -666,7 +666,7 @@
 
                     let text = '';
                     if(!this.fieldIsValid(this.fields[i])){
-                        text = '?';
+                        this.fields[i].id;
                     }else if(this.fields[i].name){
                         text = this.fields[i].name;
                     }else{
